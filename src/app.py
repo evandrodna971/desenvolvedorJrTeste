ecaccb9a16ea38635bea6dd57c1b99ad92ad5d09
evandrodna971 <evandrodna971@gmail.com>
2026-04-24 20:19:06 -0300

    Ajustes no texto

--- src/app.py
+++ src/app.py
@@ -6,32 +6,32 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Pega a URL do banco das variaveis de ambiente caso encontre (como passamos do docker-compose)
-# Se não achar nada (tipo num teste rodando seco no Python localmente), vai bater nessa string ali 
+# Se não achar nada (tipo num teste rodando no Python localmente), usa a string 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://dev_user:dev_password@localhost:5432/vagas_db")
 
 
 def get_db_connection():
     """
-    Essa função é responsável por simplesmente abrir as portinhas pro Postgres.
+    Essa função é responsável conectar ao Postgres.
     """
     try:
-        # Usa o RealDictCursor pq facilita a minha vida, ao invés de buscar os dados numa lista maluca (1, 'Vaga', etc), 
-        # esse pacote já estrutura num dicionário {'id': 1, 'nome': 'Vaga'} para enviar pra API ficar igual ao JSON.
+        # RealDictCursor facilita a minha vida, ao invés de buscar os dados numa lista (1, 'Vaga', etc), 
+        # esse pacote já estrutura num dicionário {'id': 1, 'nome': 'Vaga'} para enviar para a API ficar igual ao JSON.
         conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
         return conn
     except Exception as e:
-        print(f"B.O na hora de conectar lá banco de dados: {e}")
+        print(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
 def criar_tabela():
     """
     Cria a tabela no banco quando o programa subir
-    Achei util fazer isso pra gente não ter que rodar nenhum script SQL externo.
-    O código inicia e caso nao exista, a tabela pipoca por conta própria no bd.
+    Achei util fazer isso para não ter que rodar nenhum script SQL externo.
+    O código inicia e caso nao exista, a tabela aparece por conta própria no bd.
     """
     conn = get_db_connection()
     if conn is None:
-        print("Atenção: Não conseguiu conectar. Confere se o BD já terminou processo de Start!")
+        print("Atenção: Não conectou. Confira se o BD já terminou processo de Start!")
         return
         
     try:
@@ -48,33 +48,32 @@ def criar_tabela():
         cursor.execute(query)
         conn.commit() # consolida criação
     except Exception as e:
-        print(f"Deu erro ao criar a tabela viu: {e}")
+        print(f"Erro ao criar tabela: {e}")
     finally:
         # Importante fechar sempre para não sobrar conexões vazando memoria e travando o postgres
         cursor.close()
         conn.close()
 
-# Chamando lá em cima na largada da máquina
+# Inicia a criação da tabela
 criar_tabela()
 
-
+# Rota raiz só para ver se o server está vivo
 @app.route('/', methods=['GET'])
 def index():
-    # Rota raiz só ver se o server está pendurado vivo
-    return jsonify({"mensagem": "API de Vagas rodando tranquilamente!"}), 200
+    return jsonify({"mensagem": "API de Vagas Ativa!"}), 200
 
 @app.route('/vagas', methods=['POST'])
 def criar_vaga():
     # Puxa no Request o json do usuário
     dados = request.get_json()
     
-    # Validação pequena ali pra não criar sem nada no banco vazio que é feio
+    # Validação pequena ali para não criar sem nada no banco
     if not dados or 'empresa' not in dados or 'cargo' not in dados:
-        return jsonify({"erro": "Vish, esqueceu algo! 'empresa' e 'cargo' são campos obrigatórios."}), 400
+        return jsonify({"erro": "Esqueceu algo! 'empresa' e 'cargo' são campos obrigatórios."}), 400
         
     empresa = dados['empresa']
     cargo = dados['cargo']
-    status = dados.get('status', 'Enviado') # se nao botar o cara mandou enviou apenas
+    status = dados.get('status', 'Enviado')
     data_aplicacao = dados.get('data_aplicacao', None) 
     
     conn = get_db_connection()
@@ -84,9 +83,9 @@ def criar_vaga():
     cursor = conn.cursor()
     
     try:
-        # Protegendo usando o %s (parametros bindables) pra ninguém jogar "DROP TABLE" la e foder meu sistema (SQL inject)
+        # Protegendo usando o %s (parametros bindables) para ninguém usar SQL injection
         if data_aplicacao:
-            # O RETURNING id é um bizu massa! Assim eu já gravo e pego de volta que a DB escolheu qual foi ID gerado sem fazer outro SELECT p achar
+            # O RETURNING id retorna o id gerado pelo banco
             comando = "INSERT INTO vagas (empresa, cargo, status, data_aplicacao) VALUES (%s, %s, %s, %s) RETURNING id;"
             cursor.execute(comando, (empresa, cargo, status, data_aplicacao))
         else:
@@ -94,7 +93,7 @@ def criar_vaga():
             cursor.execute(comando, (empresa, cargo, status))
             
         id_inserido = cursor.fetchone()['id']
-        conn.commit() # Salva p sempre
+        conn.commit()
         
         return jsonify({
             "mensagem": "Vaga registrada na base com sucesso!",
@@ -103,14 +102,14 @@ def criar_vaga():
         
     except Exception as e:
         conn.rollback() # Se estourar algum erro no meio de tudo em cima ali, aborta transação do banco 
-        return jsonify({"erro": f"Erro interno brabo: {str(e)}"}), 500
+        return jsonify({"erro": f"Erro interno: {str(e)}"}), 500
     finally:
         cursor.close()
         conn.close()
 
 @app.route('/vagas', methods=['GET'])
 def listar_vagas():
-    # Lista de geral decrescente pelo id
+    # Lista todas as vagas decrescente pelo id
     conn = get_db_connection()
     if not conn:
         return jsonify({"erro": "Banco de dados fora do ar."}), 500
@@ -119,7 +118,7 @@ def listar_vagas():
     
     try:
         cursor.execute("SELECT * FROM vagas ORDER BY id DESC;")
-        # fechall puxa os dicio todinhos numa listona (o RealDictCursor formata por mim ali em cima antes)
+        # Puxa os dicionários todos em uma lista (o RealDictCursor formata por mim)
         vagas = cursor.fetchall() 
         return jsonify(vagas), 200
     except Exception as e:
@@ -163,14 +162,14 @@ def atualizar_vaga(vaga_id):
     cursor = conn.cursor()
     
     try:
-        # Primeiro confere que ela existe, pq n adianta tacar agua no teto.
+        # Primeiro confere que ela existe.
         cursor.execute("SELECT * FROM vagas WHERE id = %s;", (vaga_id,))
         vaga_existente = cursor.fetchone()
         
         if not vaga_existente:
             return jsonify({"erro": "Vaga não encontrada"}), 404
             
-        # Pega as informações dadas agr pela api. SE não vier algo do json, pegamos a "velha" do banco antes
+        # Pega as informações dadas pela api. Se não vier algo do json, pega a "velha" do banco antes
         empresa = dados.get('empresa', vaga_existente['empresa'])
         cargo = dados.get('cargo', vaga_existente['cargo'])
         status = dados.get('status', vaga_existente['status'])
@@ -179,7 +178,7 @@ def atualizar_vaga(vaga_id):
         cursor.execute(sql, (empresa, cargo, status, vaga_id))
         conn.commit()
         
-        return jsonify({"mensagem": "Informações atualizadas certinho!"}), 200
+        return jsonify({"mensagem": "Informações atualizadas!"}), 200
         
     except Exception as e:
         conn.rollback()
@@ -196,8 +195,8 @@ def deletar_vaga(vaga_id):
     cursor = conn.cursor()
     
     try:
-        # Um RETURNING salvador pra já ter ctz que deleta
-        # se nn ter id na resposta eh pq não achou
+        # Um RETURNING para já ter certeza que deleta.
+        # Se não tiver id na resposta é porque não achou.
         cursor.execute("DELETE FROM vagas WHERE id = %s RETURNING id;", (vaga_id,))
         retorno = cursor.fetchone()
         
@@ -215,5 +214,5 @@ def deletar_vaga(vaga_id):
         conn.close()
 
 if __name__ == '__main__':
-    # Roda p cima aqui, colado em qualquer IP assim ele alcança a rededo docker (usando 5000) de boa
+    # Colocado em qualquer IP assim ele alcança a rede do docker (usando 5000).
     app.run(host='0.0.0.0', port=5000, debug=True)
